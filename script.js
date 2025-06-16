8fc5b0ecee295f93af3d3d85394b15765f7758a4
kurisu-contreras <112989430+kurisu-Contreras@users.noreply.github.com>
2025-06-16 10:36:35 -0500

    feat(script): add increment for the variables that save score

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-console.log("Here we go again");
-console.log(getComputerChoice());
+
+
 
 function getHumanChoice() {
   humanChoice = prompt(
@@ -32,26 +32,32 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-//getHumanChoice();
+
 
 function playRound(computer, human) {
   human = human.toLowerCase();
-  console.log(`This it's the human choice ${human}`);
+ 
   //Cases where the computer wins
   if (computer == "rock" && human == "scissors") {
     console.log(`You lose, ${computer} beats ${human}`);
+    computerScore += 1;
   } else if (computer == "paper" && human == "rock") {
     console.log(`You lose, ${computer} beats ${human}`);
+    computerScore += 1;
   } else if (computer == "scissors" && human == "paper") {
     console.log(`You lose, ${computer} beats ${human}`);
+    computerScore += 1;
 
     //Cases where the human wins
   } else if (computer == "scissors" && human == "rock") {
     console.log(`You WIN, ${human} beats ${computer}`);
+    humanScore += 1;
   } else if (computer== "rock" && human == "paper") {
     console.log(`You WIN, ${human} beats ${computer}`);
+    humanScore += 1;
   } else if (computer == "paper" && human == "scissors") {
     console.log(`You WIN, ${human} beats ${computer}`);
+    humanScore += 1;
   } else {
     //Draw Case
     console.log(
@@ -59,4 +65,15 @@ function playRound(computer, human) {
     );
   }
 }
-//playRound(computerChoice,humanChoice);
+
+
+for (let index = 1; index < 6; index++) {
+    console.log(`Round ${index}
+    ----------------------------`)
+    console.log("Computer Choice: "+ getComputerChoice());
+    getHumanChoice();
+    playRound(computerChoice,humanChoice);
+    
+}
+console.log("Human Score:"+humanScore);
+console.log("Computer Score:"+computerScore);
